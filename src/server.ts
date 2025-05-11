3c9a4fad9cef86d11bc785e0c9ed696572e2210e
Fahmudul <fahmudul7890@gmail.com>
2025-05-11 18:00:17 +0600

    Added Proper server error handling

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,24 @@
 import { Server } from "http";
 import app from "./app";
 import Config from "./app/Config";
-
 async function connectDb() {
   const server: Server = app.listen(Config.port, () => {
     console.log(`Server is running on ${Config.port}`);
+  });
+
+  const closeServer = () => {
+    if (server) {
+      server.close(() => {
+        console.log("Server closed");
+      });
+      process.exit(1);
+    }
+  };
+  process.on("uncaughtException", (err) => {
+    closeServer();
+  });
+  process.on("unhandledRejection", (err) => {
+    closeServer();
   });
 }
 
